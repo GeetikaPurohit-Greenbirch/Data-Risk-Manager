90cb8426e2366537454ca8fe9677a1193d6ccd58
dev-bushra-greenbirch <bushra.mohammed@greenbirch.net>
2025-06-06 00:19:48 +0400

    sankey component added + chart

--- src/app/features/lineages/component/lineage-sankey/lineage-sankey.component.ts
+++ src/app/features/lineages/component/lineage-sankey/lineage-sankey.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { SharedModule } from "../../../shared/shared/shared.module";
+import * as Highcharts from 'highcharts';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { SharedModule } from 'src/app/features/shared/shared/shared.module';
+
+const Sankey = require('highcharts/modules/sankey');
+try {
+  Sankey(Highcharts);
+} catch (e) {
+  console.error('Charts Module Failed:', e);
+}
 
 @Component({
   selector: 'app-lineage-sankey',
   standalone: true,
-  imports: [SharedModule],
+  imports: [HighchartsChartModule, SharedModule],
   templateUrl: './lineage-sankey.component.html',
-  styleUrl: './lineage-sankey.component.scss'
+  styleUrl: './lineage-sankey.component.scss',
 })
 export class LineageSankeyComponent {
-
+  Highcharts: typeof Highcharts = Highcharts;
+  SankeyChartOptions: Highcharts.Options = {
+    title: { text: 'Simple Sankey Diagram' },
+    series: [
+      {
+        type: 'sankey',
+        keys: ['from', 'to', 'weight'],
+        data: [
+          ['A', 'B', 5],
+          ['A', 'C', 3],
+          ['B', 'D', 2],
+          ['C', 'D', 2],
+        ],
+        name: 'Sankey flow',
+      },
+    ],
+  };
 }
