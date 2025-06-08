e2f8c7c13597222f9ee3a9adff808ae2ffd95a92
dev-bushra-greenbirch <bushra.mohammed@greenbirch.net>
2025-06-08 20:36:33 +0400

    remove treegraph import and initalizing from here we dont needit since its init from maints

--- src/app/features/lineages/component/lineage-treegraph/lineage-treegraph.component.ts
+++ src/app/features/lineages/component/lineage-treegraph/lineage-treegraph.component.ts
@@ -1,39 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { SharedModule } from "../../../shared/shared/shared.module";
 import * as Highcharts from 'highcharts';
-import Treegraph from 'highcharts/modules/treegraph';
-
-// Treegraph(Highcharts);
 
 @Component({
   selector: 'app-lineage-treegraph',
   templateUrl: './lineage-treegraph.component.html',
-  styleUrl: './lineage-treegraph.component.scss'
+  styleUrl: './lineage-treegraph.component.scss',
 })
 
 export class LineageTreegraphComponent implements OnInit {
-  ngOnInit(): void {}
-  Highcharts: typeof Highcharts = Highcharts;
-// TreegraphChartOptions: Highcharts.Options = {
-//     title: { text: 'My Treegraph' },
-//     series: [
-//       {
-//         type: 'treegraph',
-//         data: [
-//           { id: 'Root' },
-//           { id: 'Child1', parent: 'Root' },
-//           { id: 'Child2', parent: 'Root' },
-//           { id: 'Grandchild1', parent: 'Child1' },
-//         ],
-//         dataLabels: {
-//           style: {
-//             fontSize: '13px',
-//           },
-//         },
-//         marker: {
-//           radius: 8,
-//         },
-//       },
-//     ],
-//   };
+    Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {};
+
+  ngOnInit(): void {
+    this.chartOptions = {
+      chart: { type: 'treegraph' },
+      title: { text: 'شجرة النسب' },
+      series: [{
+        type: 'treegraph',
+        keys: ['id', 'parent', 'name'],
+        data: [
+          ['A', '', 'الجد الأعلى'],
+          ['B', 'A', 'الابن الأول'],
+          ['C', 'A', 'الابن الثاني'],
+          ['D', 'B', 'الحفيد الأول'],
+          ['E', 'C', 'الحفيد الثاني']
+        ],
+        dataLabels: {
+          // formatter() { return this.point.name; } // i commented this line to avoid error
+        }
+      }]
+    };
+  }
 }
