532250585063b3739edd96838eec43b82b640529
dev-bushra-greenbirch <bushra.mohammed@greenbirch.net>
2025-06-08 20:34:52 +0400

    add hicharts modules treemap and treegrahp in main.ts and call it in order

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import * as Highcharts from 'highcharts';
+
+// استيراد الموديولات من المسار الكامل مع الامتداد .js
+import TreemapModule from 'highcharts/modules/treemap.js';
+import TreegraphModule from 'highcharts/modules/treegraph.js';
+
+// لازم تفعّل الـ Treemap قبل الـ Treegraph
+TreemapModule(Highcharts);
+TreegraphModule(Highcharts);
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,5 +18,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
