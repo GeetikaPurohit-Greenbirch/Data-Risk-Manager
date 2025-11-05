35c543a779388ad863bf7f14046a6b2b9c66b033
Manish <manish.pandey@greenbirch.net>
2025-11-05 16:59:43 +0530

    apply loader

--- src/app/features/targets/component/edit-target/edit-target.component.ts
+++ src/app/features/targets/component/edit-target/edit-target.component.ts
@@ -29,6 +29,7 @@ export class EditTargetComponent {
   targetForm!: FormGroup;
   targetUseCaseForm!: FormGroup;
   showDataFields = true;
+  loader=false;
   //showDataQuality = false;
   //showDataFieldsTable = true;
   targetTypeOptions = ['SYSTEM', 'FILE', 'DISPLAY', 'PRINTER']
@@ -318,23 +319,26 @@ export class EditTargetComponent {
     }
 
     if (this.targetId > 0) {
+      this.loader=true;
       // Step 2: Fetch data from API and patch to form
       this.targetService.getTargetById(this.targetId).subscribe({
         next: (res: any) => {
           const data = res.targetEntity;
           this.targetForm.patchValue(data);
           this.toggleFieldsBasedOnQoS(data.quality_of_service);
-
+          this.loader=false;
         },
         error: (err: any) => {
           console.error('Failed to load target:', err);
+           this.loader=false;
         }
+       
       });
       // this.generateTimeOptions();
 
-      setTimeout(() => {
-        this.cdr.detectChanges(); // ensure UI updates  
-      }, 100);
+      // setTimeout(() => {
+      //   this.cdr.detectChanges(); // ensure UI updates  
+      // }, 100);
 
       this.formLoaded = true; // triggers re-render
 
